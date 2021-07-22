6a6d13d67bc01065b2c932e7c9d0e17c10da0e00
zhyq <1064639871@qq.com>
2021-07-22 11:33:38 +0800

    配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off"
   },
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <el-button @click="onBtnClick">默认按钮</el-button>
+    <form-dialog v-bind:dialogVisible="visible"></form-dialog>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import FormDialog from "@/components/FormDialog.vue";
+export default defineComponent({
+  components: { FormDialog },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    onBtnClick() {
+      debugger;
+      this.visible = true;
+    },
+  },
+});
+</script>
