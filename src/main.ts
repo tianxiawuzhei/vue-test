10b7a5559a808b95e6b71b1ebfc73d27b8dc8666
zhyq <1064639871@qq.com>
2021-08-02 17:00:36 +0800

    悬浮球控件
    悬浮球

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+// import dragBall from "vue-drag-ball";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+// Vue.use(dragBall);
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,27 @@
   <div>
     <el-button @click="onBtnClick">默认按钮</el-button>
     <form-dialog ref="formDialog"></form-dialog>
+    <div>
+      <draggable
+        :distanceRight="100"
+        :distanceBottom="100"
+        :isScrollHidden="false"
+        :isCanDraggable="true"
+      >
+        <div style="height: 50px; width: 50px; background: black">test</div>
+      </draggable>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api";
 import FormDialog from "@/components/FormDialog.vue";
+import Draggable from "@/components/Draggable.vue";
 import * as mockdata from "@/mock/mockdata";
 type TFormDialog = InstanceType<typeof FormDialog>;
 export default defineComponent({
-  components: { FormDialog },
+  components: { FormDialog, Draggable },
   data() {
     return {};
   },
